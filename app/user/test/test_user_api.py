5cc973dfcbf7ba0f8a5163763f9121e1a10c56e3
John Laurence Burgos <johnlaurenceburgos@gmail.com>
2024-07-28 16:22:56 +0800

    user creation and flake8 update

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -28,7 +28,7 @@ class PublicUserApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn('password',res.data)
+        self.assertNotIn('password', res.data)
 
     def test_user_with_email_exist_error(self):
         payload = {
